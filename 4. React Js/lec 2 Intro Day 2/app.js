9763bffd51f150abec9ef0a99522aa2f52e6cdb1
attriashwani09 <attriashwani09@gmail.com>
2026-06-03 12:53:58 +0530

    Intro day - 2

---
+++ 4. React Js/lec 2 Intro Day 2/app.js
@@ -0,0 +1,9 @@
+const element = React.createElement("h1" , {} , "Hello Coder Army ") ; 
+
+// ReactDOM.render( element , document.querySelector("#root")) ; 
+
+// CReate rootLoc and then render 
+
+const root = ReactDOM.createRoot(  document.querySelector("#root") ) ; 
+
+root.render( element ) ; 
